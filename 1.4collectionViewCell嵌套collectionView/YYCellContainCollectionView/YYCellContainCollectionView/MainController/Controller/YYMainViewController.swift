4a64d99dc64e5ece938c0a8d2436887fe3d9dd85
杨世川 <1434636181@qq.com>
2021-10-19 10:38:28 +0800

    增加排序功能

--- 1.4collectionViewCell嵌套collectionView/YYCellContainCollectionView/YYCellContainCollectionView/MainController/Controller/YYMainViewController.swift
+++ 1.4collectionViewCell嵌套collectionView/YYCellContainCollectionView/YYCellContainCollectionView/MainController/Controller/YYMainViewController.swift
@@ -15,6 +15,8 @@ let SCREEN_HEIGHT = UIScreen.main.bounds.size.height
 
 class YYMainViewController: UIViewController {
     //MARK: - 属性 -
+    //MARK:oldIndexPath(这个主要用来辨别是否有cell被长按,不需要更新位置)
+    var dragingIndexPath: IndexPath?
     //MARK:数据源
     lazy var dataSource: Array<YYDeviceModel> = Array<YYDeviceModel>()
     
@@ -54,9 +56,70 @@ class YYMainViewController: UIViewController {
             self.dataSource.append(deviceModel)
         }
         self.listCollectionView.reloadData()
+        //2、添加手势
+        let longPress = UILongPressGestureRecognizer.init(target: self, action: #selector(longPressEvent))
+        self.listCollectionView.addGestureRecognizer(longPress)
     }
     
     //MARK: - Event Response -
+    //MARK:处理长按手势
+    @objc func longPressEvent(longPress: UILongPressGestureRecognizer) -> () {
+        //强制将cell高度设置为210
+        var isMove: Bool = true
+        for deviceModel in self.dataSource {
+            if deviceModel.subCollectionViewHeight > 210 {
+                isMove = false
+                break
+            }
+        }
+        if !isMove {
+            print("不能移动========P")
+            for deviceModel in self.dataSource {
+                deviceModel.subCollectionViewHeight = 210
+                self.listCollectionView.reloadData()
+            }
+            return
+        }
+        //获取长按点位置
+        let point = longPress.location(in: self.listCollectionView)
+        //手势处理
+        switch longPress.state {
+        case .began://手势开始
+            let indexPath = self.listCollectionView.indexPathForItem(at: point)
+            if indexPath == nil {
+                return
+            }
+            self.dragingIndexPath = indexPath
+            //iOS9以上移动cell方法
+            self.listCollectionView.beginInteractiveMovementForItem(at: indexPath!)
+            break
+        case .changed://手势移动
+            if self.dragingIndexPath == nil {
+                return
+            }
+            //更新Cell位置
+            self.listCollectionView.updateInteractiveMovementTargetPosition(point)
+            break
+        case .ended://手势结束
+            if self.dragingIndexPath == nil {
+                return
+            }
+            //cell结束移动
+            self.listCollectionView.endInteractiveMovement()
+            self.dragingIndexPath = nil
+            //重新刷新列表
+            self.listCollectionView.reloadData()
+            break
+        default:
+            if self.dragingIndexPath == nil {
+                return
+            }
+            //cell取消移动
+            self.listCollectionView.cancelInteractiveMovement()
+            self.dragingIndexPath = nil
+            break
+        }
+    }
     
     //MARK: - Public  Methods -
     
@@ -118,5 +181,21 @@ extension YYMainViewController: UICollectionViewDataSource{
         let deviceModel = self.dataSource[indexPath.row]
         cell.setDeviceModel(deviceModel: deviceModel,indexPath: indexPath,collectionView: self.listCollectionView)
         return cell
+    }
+    
+    //MARK:允许移动
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    //移动结束会调用此方法,会重新读取数据源数据
+    //所以要处理数据源数据
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        //获取移动row中数据
+        let sourceTitle = self.dataSource[sourceIndexPath.row]
+        //数据源移除该数据
+        self.dataSource.remove(at: sourceIndexPath.row)
+        //插入目标位置
+        self.dataSource.insert(sourceTitle, at: destinationIndexPath.row)
     }
 }
